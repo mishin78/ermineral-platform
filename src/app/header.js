02fb6bd5103be29270cf24516d47e740ae852599
mishin78 <mishin78@>
2023-09-05 13:23:55 +0300

    add some colors

--- src/app/header.js
+++ src/app/header.js
@@ -1,12 +1,12 @@
 //import styles from './page.module.css'
-import styles from './styles.sass'
+import './styles.sass'
 // import Contacts from './contacts'
 
 export default function Header() {
 
     return (
-        <div className={styles.up}>
-            <h1 className={styles.titleLogo}>ERMINERAL</h1>
+        <div>
+            <h1 className='titleLogo'>ERMINERAL</h1>
         </div>
     )
 }
