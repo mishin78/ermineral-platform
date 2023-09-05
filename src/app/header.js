dece46656fb106b9d569aa85d4256e13cf3a76a5
mishin78 <mishin78@>
2023-09-05 15:16:11 +0300

    worked on header

--- src/app/header.js
+++ src/app/header.js
@@ -1,12 +1,31 @@
-//import styles from './page.module.css'
 import './styles.sass'
+import Link from 'next/link'
 // import Contacts from './contacts'
 
-export default function Header() {
 
+export default function Header() {
+ 
     return (
-        <div>
+        <nav>
             <h1 className='titleLogo'>ERMINERAL</h1>
-        </div>
+            <ul>
+                <li>Partnership </li>
+                <span>|</span>
+                <li>About the author</li>
+                <span>|</span>
+                <li>Contacts</li>
+                <span>|</span>
+                <Link href='/about'><li>Instructions</li></Link>
+                <span>|</span>
+                <li>Recommendations</li>
+                <span>|</span>
+                <li>Game</li>
+            </ul>
+            <div className='icons'>
+                <img src='./dark.svg'/>
+                <img src='./lan.svg'/>
+            </div>
+        </nav>
     )
 }
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
-'use client'
 import styles from './page.module.css'
 import Header from './header'
+import './styles.sass'
 import Background from './backGround'
-import InstructionLink from './instructionLink'
 
 export default function Home() {
   
   return (
-    <main className={styles.main}>
+    <main>
       <Header />
-      <InstructionLink />
     </main>
   )
 }
+
+
